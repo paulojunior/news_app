28e995c277d45355fb7b9f8f790da1244e8902dc
Paulo Ferreira <paulo.ferreira@ditech.com.br>
2021-08-19 20:21:13 -0300

    Fetching news from NewsAPI

--- News/Controllers/NewsTableViewController.swift
+++ News/Controllers/NewsTableViewController.swift
@@ -10,12 +10,32 @@ import UIKit
 
 class NewsTableViewController: UITableViewController {
     
-    /*
-     let url = URL(string: "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=42c9f103af864d26b78d8c8e7c17196a")!
-     */
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationController?.navigationBar.prefersLargeTitles = true
+        
+        populateNews()
+    }
+    
+    private func populateNews() {
+        
+        let url = URL(string: "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=42c9f103af864d26b78d8c8e7c17196a")
+        
+        Observable.just(url)
+            .flatMap { url -> Observable<Data> in
+                let request = URLRequest(url: url)
+                return URLSession.shared.rx.data(request: request)
+            } . map { data -> [Article]? in
+                return try? JSONDecoder().decode(ArticleList.self, from: data).articles
+            }.subscribe(onNext: { articles in
+                
+                if let articles = articles {
+                    DispatchQueue.main.async {
+                        self?.tableView.reloadData()
+                    }
+                }
+                
+            })
+        
     }
 }
