8b169a3a7eab2433361b21148688f6c3fa019e62
Paulo Junior <jr@live.at>
2021-08-20 13:34:44 -0300

    Displaying News in UITableView

---
+++ News/Cells/ArticleTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  ArticleTableViewCell.swift
+//  News
+//
+//  Created by Junior Ferreira on 20/08/21.
+//
+
+import Foundation
+import UIKit
+
+class ArticleTableViewCell: UITableViewCell {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+}
--- News/Controllers/NewsTableViewController.swift
+++ News/Controllers/NewsTableViewController.swift
@@ -7,10 +7,12 @@
 
 import Foundation
 import UIKit
+import RxSwift
+import RxCocoa
 
 class NewsTableViewController: UITableViewController {
     
-    let disposeBah = DisposeBag()
+    let disposeBag = DisposeBag()
     
     private var articles = [Article]()
     
@@ -21,22 +23,42 @@ class NewsTableViewController: UITableViewController {
         populateNews()
     }
     
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        self.articles.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "ArticleTableViewCell", for: indexPath) as? ArticleTableViewCell else {
+            fatalError("ArticleTableViewCell does not exist")
+        }
+        
+        cell.titleLabel.text = self.articles[indexPath.row].title
+        cell.descriptionLabel.text = self.articles[indexPath.row].title
+        
+        return cell
+    }
+    
     private func populateNews() {
         
         let url = URL(string: "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=42c9f103af864d26b78d8c8e7c17196a")
         
         Observable.just(url)
             .flatMap { url -> Observable<Data> in
-                let request = URLRequest(url: url)
+                let request = URLRequest(url: url!)
                 return URLSession.shared.rx.data(request: request)
             } . map { data -> [Article]? in
                 return try? JSONDecoder().decode(ArticlesList.self, from: data).articles
             }.subscribe(onNext: { articles in
                 
                 if let articles = articles {
-                    self?.articles = articles
+                    self.articles = articles
                     DispatchQueue.main.async {
-                        self?.tableView.reloadData()
+                        self.tableView.reloadData()
                     }
                 }
                 
