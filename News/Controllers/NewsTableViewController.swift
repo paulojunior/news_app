115057614b14d7305528b7358c92d4e50bcfb8a3
Paulo Junior <jr@live.at>
2021-08-20 16:46:05 -0300

    Implementing URLRequest Observable to Perform Request

--- News/Controllers/NewsTableViewController.swift
+++ News/Controllers/NewsTableViewController.swift
@@ -44,23 +44,13 @@ class NewsTableViewController: UITableViewController {
     }
     
     private func populateNews() {
-        
-        let url = URL(string: "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=42c9f103af864d26b78d8c8e7c17196a")
-        
-        Observable.just(url)
-            .flatMap { url -> Observable<Data> in
-                let request = URLRequest(url: url!)
-                return URLSession.shared.rx.data(request: request)
-            } . map { data -> [Article]? in
-                return try? JSONDecoder().decode(ArticlesList.self, from: data).articles
-            }.subscribe(onNext: { articles in
-                
-                if let articles = articles {
-                    self.articles = articles
+        URLRequest.load(resource: ArticlesList.all)
+            .subscribe(onNext: { [weak self] result in
+             
+                    self?.articles = result.articles
                     DispatchQueue.main.async {
-                        self.tableView.reloadData()
+                        self?.tableView.reloadData()
                     }
-                }
                 
             }).disposed(by: disposeBag)
         
--- News/Models/Article.swift
+++ News/Models/Article.swift
@@ -11,6 +11,13 @@ struct ArticlesList: Decodable {
     let articles: [Article]
 }
 
+extension ArticlesList {
+    static var all: Resource<ArticlesList> = {
+       let url = URL(string: "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=42c9f103af864d26b78d8c8e7c17196a")
+        return Resource(url: url)
+    }()
+}
+
 struct Article: Decodable {
     let title: String
     let description: String
